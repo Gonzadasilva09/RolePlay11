57083c7512eb27dfd5e82a2b942f4da6585e5c64
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2021-09-17 23:20:10 -0300

    "Finalizamos con los casos de testeo"

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -32,10 +32,69 @@ namespace Test.Library
             hpexpected = 100;
 
             Assert.AreEqual(hpexpected, LegolasTest.Hp);
-
-            
-            
         }
+        public void pruebaenanos()
+        {
+            Game TestGame = new Game();
+            Dwarf Panza = new Dwarf("Fuerteychiquito",100,150,"patadamagica",170);
+            Dwarf Peleador = new Dwarf("Elvaliente",150,180,"volar",170);
+            TestGame.Atack(Panza, Peleador);
+            int hpexpected =  130;
+            Assert.AreEqual(hpexpected, Peleador.Hp);
+            TestGame.Atack(Peleador, Peleador);
+            var hpvolar =  70;
+            Assert.AreEqual(hpvolar, Panza.Hp);
+            //testeo de la clase enanos, verificación de que pueden atacar y recibir ataques los personajes
+        }
+        public void pruebaitems()
+        {
+            Game TestGame = new Game();
 
+            Item Testitem = new Item("test1",100,100,100);
+            Elves TestElves = new Elves("testeralto",0,0,"testing power",0);
+            Dwarf TestDwarf = new Dwarf("testerbajo",0,0,"testing power",0);
+            Wizard TestWizz = new Wizard("testermagico",0,0,"testing power",0);
+
+            int vida=100;
+            int daño=100;
+            int defensa=100;
+
+            TestElves.AddItem(Testitem);
+            TestDwarf.AddItem(Testitem);
+            TestWizz.AddItem(Testitem);
+
+            Assert.AreEqual(vida,TestElves.Hp);
+            Assert.AreEqual(vida,TestDwarf.Hp);
+            Assert.AreEqual(vida,TestWizz.Hp);
+
+            Assert.AreEqual(daño,TestElves.Damage);
+            Assert.AreEqual(daño,TestDwarf.Damage);
+            Assert.AreEqual(daño,TestWizz.Damage);
+
+            Assert.AreEqual(defensa,TestElves.Defense);
+            Assert.AreEqual(defensa,TestDwarf.Defense);
+            Assert.AreEqual(defensa,TestWizz.Defense);
+
+            vida=0;
+            daño=0;
+            defensa=0;
+            
+            TestElves.RemoveItem(Testitem);
+            TestDwarf.RemoveItem(Testitem);
+            TestWizz.RemoveItem(Testitem);
+
+            Assert.AreEqual(vida,TestElves.Hp);
+            Assert.AreEqual(vida,TestDwarf.Hp);
+            Assert.AreEqual(vida,TestWizz.Hp);
+
+            Assert.AreEqual(daño,TestElves.Damage);
+            Assert.AreEqual(daño,TestDwarf.Damage);
+            Assert.AreEqual(daño,TestWizz.Damage);
+
+            Assert.AreEqual(defensa,TestElves.Defense);
+            Assert.AreEqual(defensa,TestDwarf.Defense);
+            Assert.AreEqual(defensa,TestWizz.Defense);
+
+        }
     }
 }
